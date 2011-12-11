46001b6e2d9e04d3089ebbe850cec6d2836906e9
Graham Jones <graham@marx.(none)>
2011-12-11 21:08:49 +0000

    Added play/pause buttons for slideshow and audio.  Added comments to code.

--- slideshow/js/slideshow.js
+++ slideshow/js/slideshow.js
@@ -8,6 +8,9 @@ ss = {
     'timeout':5000
 };   
 
+////////////////////////////////////////////////////////////////////////
+// ss.obj2str:  Produces a string describing the members of object obj.
+//
 ss.obj2str = function(obj) {
     var retStr = '';
     for (k in obj) {
@@ -16,6 +19,12 @@ ss.obj2str = function(obj) {
     return retStr;
 }
 
+
+/////////////////////////////////////////////////////////////////////
+// ss.isImg:  Returns true if the string passed as a parameter is
+//     an image filename (based on filename extension), otherwise
+//     returns false.
+//
 ss.isImg = function(nameObj) {
     var imgList = ['.jpg','.jpeg','.png','.gif'];
     var nameStr = String(nameObj);
@@ -29,34 +38,91 @@ ss.isImg = function(nameObj) {
     return false;
 }
 
+///////////////////////////////////////////////////////////////////////////
+// ss.resizeImage
+// ss.resizeImage is called by the onLoad event when an image is loaded.
+// It re-sizes it to fit on the screen, while preserving its aspect ratio,
+// then displays the image using the jQuery slideDown effect.
+//
 ss.resizeImage = function() {
     //alert("resizeImage()");
     var width = jQuery('#photo').attr('naturalWidth');
     var height = jQuery('#photo').attr('naturalHeight');
-    var x = jQuery('#photoDiv').width();
-    var maxy = 400;
-    //if (height * x / width <= maxy)
-    //alert("resizeImage - width="+width+", height="+height+", divWidth="+x);
-    
+    var maxx = jQuery('#photoDiv').width();
+    var maxy = jQuery('#photoDiv').height();
+    var img_w = maxx;
+    var img_h = maxx * height / width;
+    var margin = 0;
+    if (img_h > maxy) {
+	img_h = maxy;
+	img_w = width * maxy / height;
+    }
+    margin = 0.5*(maxx-img_w);
+    jQuery('#photo').height(img_h);
+    jQuery('#photo').width(img_w);
+    jQuery('#photo').css('margin-left:'+margin);
+    // Now display the image.
+    jQuery('#photo').slideDown('slow');
 }
+
+
+////////////////////////////////////////////////////////////////////
+// ss.changePhoto:  Hides the current image, then loads the next 
+//    image from the list of images in the slideshow.
+//    The onLoad event will be triggered by loading the image, which
+//    calls ss.resizeImage which resizes and displays the new image.
+//
 ss.changePhoto = function () {
     //alert("ss.photoNo = " + ss.photoNo + ", Image=" + ss.imgList[ss.photoNo]);
     ss.photoNo += 1;
     if (ss.photoNo >= ss.imgList.length) {
 	ss.photoNo = 0;
     }
-    jQuery('#photo').slideUp('fast',function() {
+    jQuery('#photo').slideUp('slow',function() {
 	jQuery('#photo').attr('src',ss.imgList[ss.photoNo]);
-	jQuery('#photo').slideDown('fast');
+	// We do not re-show the image here - the onLoad event displays
+	// the image using the resizeImage() function.
+	//jQuery('#photo').slideDown('slow');
     });
     
     ss.timeoutID = window.setTimeout(ss.changePhoto,ss.timeout);
 }
 
-ss.startSlideshow = function() {
-    ss.changePhoto();
+/////////////////////////////////////////////////////////////
+// ss.toggleAudio: Toggles the audio play on or off.
+//
+ss.toggleAudio = function() {
+    var audio = $('#audioPlayer').get(0);
+    if (audio.paused) { 
+	audio.play(); 
+	jQuery('#muteButton').html('Mute');
+    } else { 
+	audio.pause();
+	jQuery('#muteButton').html('Play Music');
+    }  
 }
 
+/////////////////////////////////////////////////////////////
+// ss.toggleSlideshow: Toggles the slideshow on or off.
+//
+ss.toggleSlideshow = function() {
+    if (!ss.running) {
+	ss.changePhoto();
+	ss.running = true;
+	jQuery('#pauseButton').html('Pause');
+    } else {
+	window.clearTimeout(ss.timeoutID);
+	ss.timeoutID = -1;
+	ss.running = false;
+	jQuery('#pauseButton').html('Start');
+    }
+}
+
+
+///////////////////////////////////////////////////////////////
+// ss.initialise_slideshow:  initialises the slideshow and
+//   starts it, along with audio playback.
+//
 ss.initialise_slideshow = function() {
     var indexObj;
     var i;
@@ -64,6 +130,7 @@ ss.initialise_slideshow = function() {
     ss.timeoutID = -1;
     ss.photoNo = 1;
     ss.imgList = [];
+    ss.running = false;
     jQuery.get(ss.photoDir,'',function(data) {
 	jQuery('#photo').html('<img src="">');
 	indexObj = jQuery(data)
@@ -75,9 +142,12 @@ ss.initialise_slideshow = function() {
 	});
 	//alert("data="+data);
 	//alert(obj2str(ss.imgList));
+	jQuery('#pauseButton').click(ss.toggleSlideshow);
+	jQuery('#muteButton').click(ss.toggleAudio);
 	if (ss.imgList.length > 0) {
 	    jQuery('#photo').load(ss.resizeImage);
-	    ss.startSlideshow();
+	    ss.toggleSlideshow();
+	    ss.toggleAudio();
 	} else {
 	    alert("Oh No - can't find any photos - giving up!");
 	}
